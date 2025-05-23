2ad39f7ea54008413d080fa2570d0f1ce55ee5f7
Kamal Chaturvedi <k.chaturvedi@f5.com>
2025-05-22 17:43:52 -0700

    Added a new custom log processor, that performs gzip operation on each log record, updating the body in-place.

--- internal/collector/factories.go
+++ internal/collector/factories.go
@@ -10,6 +10,7 @@ import (
 	nginxreceiver "github.com/nginx/agent/v3/internal/collector/nginxossreceiver"
 	"github.com/nginx/agent/v3/internal/collector/nginxplusreceiver"
 
+	"github.com/nginx/agent/v3/internal/collector/logsgzipprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/prometheusexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/headerssetterextension"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/healthcheckextension"
@@ -104,6 +105,7 @@ func createProcessorFactories() map[component.Type]processor.Factory {
 		redactionprocessor.NewFactory(),
 		resourceprocessor.NewFactory(),
 		transformprocessor.NewFactory(),
+		logsgzipprocessor.NewFactory(),
 	}
 
 	processors := make(map[component.Type]processor.Factory)
